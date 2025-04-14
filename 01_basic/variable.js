cc216690f3ec40b1ee00d232586a7ce8728f55b0
anchalchau <anchalchaudhary408@gmail.com>
2025-04-14 17:00:13 +0000

    variable

---
+++ 01_basic/variable.js
@@ -0,0 +1,10 @@
+//const accountid=12345;
+//console.log(accountid);
+let accountemail="anchal@gmail.com";
+var accountpassword="123456";
+accountcity="delhi";
+
+accountemail="sandeep@gmail.com";
+accountpassword="1212121";
+accountcity="delhi";
+console.table([accountemail,accountpassword,accountcity]);
