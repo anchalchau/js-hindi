1219da698cc40f950871461e94c7dbae35c2bba5
anchalchau <anchalchaudhary408@gmail.com>
2025-04-14 17:00:27 +0000

    datatype

---
+++ 01_basic/datatype.js
@@ -0,0 +1,16 @@
+"use strict"; //it use for the new  version type of code useability
+//alert(3+3)//we are using nodejs,not browser
+//console.log(3+3) code readability should be high
+let name="anchal"
+let age= 3
+let isLoggedIn=false
+let state=null
+//number
+//bigint
+//string
+//boolean
+//null
+//undeffined
+//symbol
+ console.log(typeof null);
+ console.log(typeof undefined)
