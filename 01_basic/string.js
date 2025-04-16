09b768c4b26ba3bdfccaa97427f23d75aa9cecb6
anchalchau <anchalchaudhary408@gmail.com>
2025-04-16 18:24:16 +0000

    string

---
+++ 01_basic/string.js
@@ -0,0 +1,21 @@
+//const gamename = new String("anchal")
+//console.log(gamename)
+//concatination
+const a="hello"
+const b="anchal"
+//console.log(a+b)
+//one more example for concatination with the help of $ symbol and {} carlebraces
+console.log(`hello everyone ${a}`);
+const gamename = new String("anchal")
+// this we can use in the inside the console its give undefine after that you will go to (gamename)
+//after that it will show the the output in vertical manner
+//console.log(gamename[0]);
+//console.log(gamename.length);
+//we can access anything with the help of this (.) method
+//console.log(gamename.toUpperCase());//like that
+//console.log(gamename.slice(-8,4));
+//
+//console.log(gamename.substr(8,4));
+const a1="baby"
+//console.log(a1.replace('y','a'))
+console.log(a1.split('-'))
